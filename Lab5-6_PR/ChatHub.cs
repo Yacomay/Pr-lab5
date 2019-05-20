d1b05613c4aa7b880c0a948b95322376ac08a6a2
Corneliu Iarincovschi <cornel.iarincovschi@gmail.com>
2019-05-20 23:00:07 +0300

    Add project files.

---
+++ Lab5-6_PR/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Lab5_6_PR
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message, string connId)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.Client(connId).appendNewMessage(name, message);
+        }
+    }
+}
